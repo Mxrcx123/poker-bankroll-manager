08d4529a580c90760d17c1c2b22f58a73ca4512b
Mrnoname4 <martin.derler@gmail.com>
2026-04-08 09:28:41 +0200

    crud, model, schema & db finished + main created

---
+++ backend/app/crud/__init__.py
@@ -0,0 +1,19 @@
+from .userCrud import UserCrud
+from .cashSessionCrud import CashSessionCrud
+from .tournamentCrud import TournamentCrud
+from .bankrollEventCrud import BankrollEventCrud
+from .bankrollSnapshotCrud import BankrollSnapshotCrud
+from .sessionCrud import SessionCrud
+from .gameModeCrud import GameModeCrud
+from .platformCrud import PlatformCrud
+
+__all__ = [
+    "UserCrud",
+    "CashSessionCrud",
+    "TournamentCrud",
+    "BankrollEventCrud",
+    "BankrollSnapshotCrud",
+    "SessionCrud",
+    "GameModeCrud",
+    "PlatformCrud",
+]
---
+++ backend/app/crud/gameModeCrud.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import Session
+from ..model.game_mode import GameMode
+
+
+class GameModeCrud():
+    @staticmethod
+    def create_game_mode(db: Session, title: str):
+        new_game_mode = GameMode(
+            title=title
+        )
+        db.add(new_game_mode)
+        db.commit()
+        db.refresh(new_game_mode)
+        return new_game_mode
+
+    @staticmethod
+    def get_game_mode_by_id(db: Session, game_mode_id: int):
+        return db.query(GameMode).filter(GameMode.id == game_mode_id).first()
+
+    @staticmethod
+    def get_game_mode_by_title(db: Session, title: str):
+        return db.query(GameMode).filter(GameMode.title == title).first()
+
+    @staticmethod
+    def get_all_game_modes(db: Session):
+        return db.query(GameMode).all()
+
+    @staticmethod
+    def update_game_mode(db: Session, game_mode_id: int, title: str = None):
+        game_mode = db.query(GameMode).filter(GameMode.id == game_mode_id).first()
+        if not game_mode:
+            return None
+        if title is not None:
+            game_mode.title = title
+        db.commit()
+        db.refresh(game_mode)
+        return game_mode
+
+    @staticmethod
+    def delete_game_mode(db: Session, game_mode_id: int):
+        game_mode = db.query(GameMode).filter(GameMode.id == game_mode_id).first()
+        if game_mode:
+            db.delete(game_mode)
+            db.commit()
+        return game_mode
