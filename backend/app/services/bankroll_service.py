8722cb01be77e8c8a7885926140763e50789cc4e
HeyKathi <katharina.almer@sr.htlweiz.at>
2026-04-29 08:26:58 +0200

    Unnötigen App-Ordner gelöscht

--- backend/app/services/bankroll_service.py
+++
@@ -1,8 +0,0 @@
-from crud.withdrawal_crud import db_withdrawals
-
-def get_current_bankroll():
-    base_bankroll = 1000  # jetzt Startwert (später aus DB berechnen)
-
-    total_withdrawals = sum(w.amount for w in db_withdrawals.values())
-
-    return base_bankroll - total_withdrawals
